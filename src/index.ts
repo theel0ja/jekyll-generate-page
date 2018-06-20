b08b7c306437c5740a4b185a45a8b09aa60aa3c5
Elias Ojala <git@eliasojala.me>
2018-06-20 12:13:42 +0200

    TEMPLATE_FOLDER inside render function

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,11 @@ import Twig, { CompileOptions } from "twig";
 import IRenderData from "./interfaces/IRenderData";
 
 // Settings
-const TEMPLATE_FOLDER = path.resolve(__dirname, "..", "templates");
 
 function render(template: string, data: IRenderData) {
   return new Promise((resolve, reject) => {
+    const TEMPLATE_FOLDER = path.resolve(__dirname, "..", "templates");
+
     const fileName = path.resolve(TEMPLATE_FOLDER, `${template}.twig`);
 
     Twig.renderFile(fileName, data as any, (err, output: string) => {
